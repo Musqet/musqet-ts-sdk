4ac0ad632ac59b87f2cf52c087fcde7b8bf5ba75
gandlaf21 <gandlaf@proton.me>
2023-10-11 08:20:16 +0700

    format

--- test/unit/cryptography.test.ts
+++ test/unit/cryptography.test.ts
@@ -1,3 +1,3 @@
 test('crypto', async () => {
-    expect(true).toBe(true);
-})
+	expect(true).toBe(true);
+});
--- test/unit/musqetClass.test.ts
+++ test/unit/musqetClass.test.ts
@@ -1,3 +1,3 @@
 test('MusqetUser', async () => {
-		expect(true).toBe(true);
-	})
+	expect(true).toBe(true);
+});
